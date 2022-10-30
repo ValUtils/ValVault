dc69f1d6082506b0e243a274a0add21e9487a464
PWall <34860495+PWall2222@users.noreply.github.com>
2022-10-30 13:20:06 +0100

    fix: export snake case symbols

--- ValVault/__init__.py
+++ ValVault/__init__.py
@@ -3,7 +3,7 @@ from .structs import *
 from .riot import make_headers
 
 __all__ = [
-	"getAuth", "get_users", "get_pass",
+	"get_auth", "get_users", "get_pass",
 	"new_user", "init",
 	"User", "Auth",
 	"make_headers",
