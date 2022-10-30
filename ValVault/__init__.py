93187b9738a07ccdb650fa5777ed82ffa5daeeba
PWall <34860495+PWall2222@users.noreply.github.com>
2022-10-30 14:47:55 +0100

    fix: remove auth related symbols from export

--- ValVault/__init__.py
+++ ValVault/__init__.py
@@ -1,10 +1,8 @@
 from .auth import *
 from .structs import *
-from .riot import make_headers
 
 __all__ = [
 	"get_auth", "get_users", "get_pass",
 	"new_user", "init",
 	"User", "Auth",
-	"make_headers",
 ]
